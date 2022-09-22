ba77ab75b7159e99025f462fff973b203868b209
Adam Tomanovich <adamtomanovich@comcast.net>
2022-09-22 14:48:28 -0500

    initial commit

---
+++ src/main/java/com/Galvanize/CrudRecipe/CrudRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.Galvanize.CrudRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRecipeApplication.class, args);
+	}
+
+}
